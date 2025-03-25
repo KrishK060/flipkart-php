b7b8701024d4fab8905111deeecab07bb89d0806
Krishk060 <krishkalaria0601@gmail.com>
2025-03-25 18:39:35 +0530

    created a update category.php

--- backend/update-category.php
+++ backend/update-category.php
@@ -1,1 +1,12 @@
 <?php
+require './db/connection.php';
+$sql = "update category set category_name=? where category_id=?";
+if($_SERVER['REQUEST_METHOD']=='POST'){
+$stmp = $conn->prepare($sql);
+$stmp->bind_param('s',$category_name);
+if($stmp->execute()){
+    echo "updated succesfully";
+}else{
+    echo "not updated";
+}
+}
