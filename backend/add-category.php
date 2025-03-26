b25ad87bd68568a8b75102501dc6d6ee91c4f3c5
Krishk060 <krishkalaria0601@gmail.com>
2025-03-26 14:45:29 +0530

    implemented the edit and delete category on the category page

--- backend/add-category.php
+++ backend/add-category.php
@@ -2,9 +2,14 @@
 require './db/connection.php';
 if($_SERVER['REQUEST_METHOD']=='POST'){
     $category_name = htmlspecialchars($_POST["cname"]);
-    $sql = 'insert into category(category_name)values(?)';
-    $stmp = $conn->prepare($sql);
-    $stmp->bind_param('s',$category_name);
+    $id = isset($_POST['category_id'])?intval($_POST['category_id']):0;
+
+   
+      $sql = 'insert into category(category_name)values(?)';
+      $stmp = $conn->prepare($sql);
+      $stmp->bind_param('s',$category_name);
+    
+   
      if($stmp->execute()){
         echo "category added succesfully";
          header("location:./add-category.php");
